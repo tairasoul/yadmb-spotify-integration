c69f2796f8a3dd39289e80d9741b9bb17ccb0e50
tairasoul <100938045+tairasoul@users.noreply.github.com>
2024-03-15 20:00:07 +0200

    more proper caching

--- src/resolvers/pager.ts
+++ src/resolvers/pager.ts
@@ -1,6 +1,6 @@
 import { PagerResolver } from "yadmb-types/types/addonTypes";
 import reg from "../defaultRegex.js";
-import playdl, { SpotifyTrack, YouTubeVideo } from "play-dl/dist/index.js";
+import playdl, { SpotifyAlbum, SpotifyPlaylist, SpotifyTrack, YouTubeVideo } from "play-dl/dist/index.js";
 import humanizeDuration from "humanize-duration";
 import * as builders from "@oceanicjs/builders";
 
@@ -23,20 +23,39 @@ export const pager: PagerResolver = {
     async available(url) {
         return reg.test(url);
     },
-    async queuedPager(track, index) {
-        if (playdl.is_expired()) await playdl.refreshToken();
+    async queuedPager(track, index, cache, invalidation) {
+        const id = (new URL(track.tracks[0].url)).pathname;
+        if (invalidation)
+            await cache.uncache("spotify-qpager-data", id);
+        const data = await cache.get("spotify-qpager-data", id);
         const embed = new builders.EmbedBuilder();
         embed.setTitle(track.name);
-        embed.addField("index", index.toString(), true);
-        embed.addField("type", track.type, true);
-        embed.addField("songs", track.tracks.length.toString(), true);
-        const sp = await playdl.spotify(track.tracks[0].url);
-        let thumbnail = sp.thumbnail?.url;
-        if (thumbnail == null) {
-            const search = await playdl.search(sp.name, {limit: 0});
-            thumbnail = getHighestResUrl(search[0]);
+        if (data) {
+            embed.addField("index", index.toString(), true);
+            embed.addField("type", track.type, true);
+            embed.addField("songs", track.tracks.length.toString(), true);
+            embed.setImage(data.extra.thumbnail);
         }
-        embed.setImage(thumbnail);
+        else {
+            if (playdl.is_expired()) await playdl.refreshToken();
+            embed.addField("index", index.toString(), true);
+            embed.addField("type", track.type, true);
+            embed.addField("songs", track.tracks.length.toString(), true);
+            const sp = await playdl.spotify(track.tracks[0].url) as SpotifyPlaylist || SpotifyAlbum;
+            let thumbnail = sp.thumbnail?.url;
+            if (thumbnail == null) {
+                const search = await playdl.search(`${sp.name} - ${sp.owner.name}`, {limit: 0});
+                thumbnail = getHighestResUrl(search[0]);
+            }
+            embed.setImage(thumbnail);
+            await cache.cache("spotify-qpager-data", {
+                id,
+                title: track.name,
+                extra: {
+                    thumbnail
+                }
+            })
+        }
         return {
             id: track.name,
             type: track.type,
@@ -44,19 +63,39 @@ export const pager: PagerResolver = {
             embed
         }
     },
-    async trackPager(track, index) {
-        if (playdl.is_expired()) await playdl.refreshToken();
+    async trackPager(track, index, cache, invalidation) {
+        const id = (new URL(track.url)).pathname;
+        if (invalidation)
+            await cache.uncache("spotify-pager-data", id);
+        const data = await cache.get("spotify-pager-data", id);
         const embed = new builders.EmbedBuilder();
         embed.setTitle(track.name);
-        const sp = await playdl.spotify(track.url) as SpotifyTrack;
-        let thumbnail = sp.thumbnail?.url;
-        if (thumbnail == null) {
-            const search = await playdl.search(sp.name, {limit: 0});
-            thumbnail = getHighestResUrl(search[0]);
+        if (data) {
+            embed.addField("Author(s)", data.extra.artists.join());
+            embed.addField("Duration", humanizeDuration(data.extra.durationInSec * 1000));
+            embed.setImage(data.extra.thumbnail);
         }
-        embed.setImage(thumbnail);
-        embed.addField("Author", sp.artists.join());
-        embed.addField("Duration", humanizeDuration(sp.durationInSec * 1000));
+        else {
+            if (playdl.is_expired()) await playdl.refreshToken();
+            const sp = await playdl.spotify(track.url) as SpotifyTrack;
+            embed.addField("Author(s)", sp.artists.map((artist) => artist.name).join());
+            embed.addField("Duration", humanizeDuration(sp.durationInSec * 1000));
+            let thumbnail = sp.thumbnail?.url;
+            if (thumbnail == null) {
+                const search = await playdl.search(`${sp.name} - ${sp.artists.join()}`, {limit: 0});
+                thumbnail = getHighestResUrl(search[0]);
+            }
+            embed.setImage(thumbnail);
+            await cache.cache("spotify-pager-data", {
+                id,
+                title: track.name,
+                extra: {
+                    thumbnail,
+                    artists: sp.artists.map((artist) => artist.name),
+                    durationInSec: sp.durationInSec
+                }
+            })
+        }
         return {
             id: track.name,
             type: "song",
--- src/resolvers/song.ts
+++ src/resolvers/song.ts
@@ -8,12 +8,30 @@ export const songData: dataResolver = {
         return reg.test(url);
     },
     priority: 0,
-    async resolve(url) {
+    async resolve(url, cache, invalidation) {
+        const s_url = new URL(url);
+        const s_id = s_url.pathname;
+        if (invalidation)
+            await cache.uncache("spotify-song-data", s_id);
+        const data = await cache.get("spotify-song-data", s_id);
+        if (data) {
+            return {
+                url: data.extra.url,
+                title: data.title
+            }
+        }
         if (playdl.is_expired()) await playdl.refreshToken();
         const sp = await playdl.spotify(url);
         if (!(sp instanceof SpotifyTrack)) {
             return `Spotify url ${url} is not a spotify track!`;
         }
+        await cache.cache("spotify-song-data", {
+            id: s_id,
+            title: sp.name,
+            extra: {
+                url: sp.url
+            }
+        })
         return {
             url: sp.url,
             title: sp.name
